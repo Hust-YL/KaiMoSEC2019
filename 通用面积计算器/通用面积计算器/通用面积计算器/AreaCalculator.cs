719f3a922fde7d4f0f4586e0d13471c81f5534b8
724364708@qq.com <724364708@qq.com>
2019-08-08 20:22:02 +0800

    SKK Functions Commit

---
+++ 通用面积计算器/通用面积计算器/通用面积计算器/AreaCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 通用面积计算器
+{
+    public class AreaCalculator
+    {
+        float TriangleThree(float x, float y, float z)
+        {
+            float cir = x+y+z;
+            float area = Math.Sqrt(cir / 2 * (cir / 2 - x) * (cir / 2 - y) * (cir / 2 - z));
+            return area = (float)Math.Round(area, 3);
+        }
+        float TriangleTwo(float x, float y)
+        {
+            float area = 0.5 * x * y;
+            return area = (float)Math.Round(area, 3);
+        }
+        float Rectangle(float a,float b)
+        {
+            float area = a * b;
+            return area = (float)Math.Round(area, 3);
+        }
+        float Circle(float r)
+        {
+            float area = Math.PI * r * r;
+            return area = (float)Math.Round(area, 3);
+        }
+    }
+}
---
+++ 通用面积计算器/通用面积计算器/通用面积计算器/InchToCentimetre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 通用面积计算器
+{
+    public class InchToCentimetre
+    {
+        float Transform(float x)
+        {
+            return x * 2.54;
+        }
+    }
+}
